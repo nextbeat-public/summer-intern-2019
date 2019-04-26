46682b5659f9505bf9b940a9ad72e76347be178f
Yoshinobu Kinugasa <yoshinobu.kinugasa@nextbeat.net>
2019-04-26 20:56:27 +0900

    ユーザ登録ページの雛形を追加

---
+++ app/controllers/app/NewUser.scala
@@ -0,0 +1,27 @@
+/*
+ * This file is part of Nextbeat services.
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
+package controllers.app
+
+import play.api.i18n.I18nSupport
+import play.api.mvc.{ AbstractController, MessagesControllerComponents }
+import model.component.util.ViewValuePageLayout
+
+// 登録: 新規ユーザー
+//~~~~~~~~~~~~~~~~~~~~~
+class NewUserController @javax.inject.Inject()(
+  cc: MessagesControllerComponents
+) extends AbstractController(cc) with I18nSupport {
+
+  /**
+   * ページの表示
+   */
+  def viewForApp = Action { implicit request =>
+    val vv = ViewValuePageLayout(id = request.uri)
+    Ok(views.html.site.app.new_user.Main(vv))
+  }
+}
